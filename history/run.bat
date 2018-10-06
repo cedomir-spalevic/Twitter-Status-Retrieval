2c7374446bb033e029a9e37e95c434d1b2a2aa8a
cedomir-spalevic <cedomir.spalevic@gmail.com>
2018-10-05 20:51:12 -0500

    Update run.bat

--- history/run.bat
+++ history/run.bat
@@ -3,6 +3,6 @@ SET username=%1
 SET ending_date=%2
 SET beginning_date=%3
 SET classpath=".;vendor\twitter4j-core-4.0.4.jar;vendor\twitter4j-stream-4.0.4.jar;vendor\json-simple-1.1.1.jar;"
-javac -cp %classpath% History.java
+javac -cp %classpath% History.java %username%
 java -cp %classpath% History %username% %ending_date% %beginning_date%
 del *.class /S
